1ac62ea98901096d500bcdf4a941c139e2ff1976
Hanna Skairipa <hanna_boone@icloud.com>
2024-01-19 13:43:39 -0700

    Initial Commit

---
+++ Level 1,2,3/Level_1_2_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Level_1_2_3App.swift
+//  Level 1,2,3
+//
+//  Created by Boone, Hanna - Student on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Level_1_2_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
